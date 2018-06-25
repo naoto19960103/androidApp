adf9759706ce425289e653ea364538b89a2ecd63
Naoto Nishimura <nnskskr960103@yahoo.co.jp>
2018-06-26 00:10:36 +0900

    スタート画面から選択画面まで画面遷移できるようにした

--- Android Application/Keiba Database/app/src/main/java/com/websarva/wings/android/keibadatabasesample/KeibaSampleActivity.java
+++ Android Application/Keiba Database/app/src/main/java/com/websarva/wings/android/keibadatabasesample/KeibaSampleActivity.java
@@ -1,7 +1,9 @@
 package com.websarva.wings.android.keibadatabasesample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class KeibaSampleActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class KeibaSampleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_keiba_sample);
+    }
+    public void onStartButton(View view){
+        //インテントオブジェクト
+        Intent i = new Intent(KeibaSampleActivity.this,SelectHorseActivity.class);
+        startActivity(i);
     }
 }
--- Android Application/Keiba Database/app/src/main/java/com/websarva/wings/android/keibadatabasesample/SelectHorseActivity.java
+++ Android Application/Keiba Database/app/src/main/java/com/websarva/wings/android/keibadatabasesample/SelectHorseActivity.java
@@ -2,12 +2,20 @@ package com.websarva.wings.android.keibadatabasesample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class SelectHorseActivity extends AppCompatActivity {
 
+    ListView horseList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_horse);
+
+        //馬名リストの表示
+        horseList = findViewById(R.id.lvSelectHorse);
+
+
     }
 }
